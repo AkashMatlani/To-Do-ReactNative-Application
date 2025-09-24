4efff311b0c27c5816f7e4a8c8967cdf867717f1
Akash Matlani <aakash.matlani@yahoo.com>
2025-09-24 14:18:46 +0100

    Delete & Update Query Added

--- convex/todos.ts
+++ convex/todos.ts
@@ -1,5 +1,5 @@
 
-import { v } from 'convex/values';
+import { ConvexError, v } from 'convex/values';
 import { mutation, query } from './_generated/server';
 
 export const getTodos= query({
@@ -9,15 +9,35 @@ export const getTodos= query({
     },
 });
 
-export const addTodos =mutation({
+export const addTodos = mutation({
     args:{text:v.string()},
     handler: async(ctx, args) =>{
-       const todoId= await ctx.db.insert("todos",{
+       const todoId = await ctx.db.insert("todos",{
             text: args.text,
             isCompleted:false
         });
-
         return todoId;
     },
-
 });
+
+export const toggleTodos = mutation({
+    args:{id: v.id("todos")},
+    handler: async(ctx,args)=>{
+        const todo = await ctx.db.get(args.id)
+        if(todo!) throw new ConvexError("Todo not Found")
+
+            await ctx.db.patch(args.id,{
+                isCompleted: !todo!.isCompleted
+            })
+        }
+});
+
+export const deleteTodo = mutation({
+
+    args :{id: v.id("todos")},
+    handler: async (ctx, args)=>
+    {
+      await ctx.db.delete(args.id);
+    },
+});
+
