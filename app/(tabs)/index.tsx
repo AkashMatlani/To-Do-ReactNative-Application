66c0a75e1b3987e2ffa3cae05b35e5dff879c5d4
Akash Matlani <aakash.matlani@yahoo.com>
2025-09-25 15:10:27 +0100

    Linear Gradient added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,22 @@
 import { createHomeStyle } from "@/assets/styles/home.style";
 import useTheme from "@/hooks/useTheme";
+import { LinearGradient } from 'expo-linear-gradient';
 import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
 
   const homestyle = createHomeStyle(colors);
   return (
-  <SafeAreaView>
-    <Text style={homestyle.container}>Edit app/index.tsx to edit this screen. sky One</Text>
-    <TouchableOpacity onPress={toggleDarkMode}>
-      <Text>Toggle Button</Text>
-    </TouchableOpacity>
-  </SafeAreaView>
+    <LinearGradient colors={colors.gradients.background} style={homestyle.container}>
+      <SafeAreaView>
+        <Text style={homestyle.container}>Edit app/index.tsx to edit this screen. sky One</Text>
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>Toggle Button</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
