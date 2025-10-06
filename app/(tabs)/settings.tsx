3ce5701184ca2128f019518aa82b155cab800304
Akash Matlani <aakash.matlani@yahoo.com>
2025-10-06 13:06:07 +0100

    Setting Screen Header Added

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { createHomeStyle } from '@/assets/styles/home.style';
+import useTheme from '@/hooks/useTheme';
+import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
 
 const SettingScreen = () => {
+
+  const [isAutoSync, setIsAutoSync] = useState(true);
+  const [isNotificationEnable, setIsNotificationEnable] = useState(true);
+
+  const { colors, isDarkMode, toggleDarkMode } = useTheme();
+
+  const SettingsStyle = createHomeStyle(colors);
+
   return (
-    <SafeAreaView>
-      <Text>SettingScreen</Text>
-    </SafeAreaView>
+    <LinearGradient colors={colors.gradients.background} 
+    style={SettingsStyle.container}>
+      <SafeAreaView style={SettingsStyle.safeArea}>
+        {/* Header Component */}
+        <View style={SettingsStyle.header}>
+          <View style={SettingsStyle.titleContainer}>
+            <LinearGradient colors={colors.gradients.primary}
+            style ={SettingsStyle.iconContiner}>
+              <Ionicons name='settings' size={30} color="#ffffff"></Ionicons>
+            </LinearGradient>
+            <Text style={SettingsStyle.title}>Settings</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   )
 }
 
