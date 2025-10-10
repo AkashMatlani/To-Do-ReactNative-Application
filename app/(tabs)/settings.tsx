da027943bf018cf5a774884116e4d337cab74c9e
Akash Matlani <aakash.matlani@yahoo.com>
2025-10-10 15:55:52 +0100

    Auto Sync added

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -4,7 +4,6 @@ import ProgressStats from '@/components/ProgressStats';
 import useTheme from '@/hooks/useTheme';
 import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
-import React from 'react';
 import { ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
